4a5f5f81b9bd2af12c32de3ccd0130795c298e31
Heedong Kim <ruthetum@gmail.com>
2020-10-14 03:29:38 +0900

    [feat] 크롤링 결과 제공

--- scrap/views.py
+++ scrap/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 def home(req):
-    return render(req, 'home.html', {})
+    notices = Notice.objects.all()
+    for notice in notices:
+        rep = notice.link.replace('&amp;', '&')
+        notice.link = rep
+        notice.save()
+    return render(req, 'home.html', {'notices': notices})
