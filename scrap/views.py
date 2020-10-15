d1f54a1c187ce9ee68c464003ff0ac9962023b3f
Heedong Kim <ruthetum@gmail.com>
2020-10-16 00:15:30 +0900

    [feat] html 변경

--- scrap/views.py
+++ scrap/views.py
@@ -7,4 +7,11 @@ def home(req):
         rep = notice.link.replace('&amp;', '&')
         notice.link = rep
         notice.save()
-    return render(req, 'home.html', {'notices': notices})
+
+    notices1 = Notice.objects.all()[:10]
+    notices2 = Notice.objects.all()[10:20]
+    notices3 = Notice.objects.all()[20:30]
+    notices4 = Notice.objects.all()[30:]
+
+    content = {'notices1': notices1, 'notices2': notices2, 'notices3': notices3, 'notices4': notices4,}
+    return render(req, 'home.html', content)
